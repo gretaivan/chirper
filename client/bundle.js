66ecf1bf9ad9aa4857ac838d439840862d3c67ce
gretaivan <greta.ivan91@gmail.com>
2021-03-12 16:55:10 +0000

    initial commit

---
+++ client/bundle.js
@@ -0,0 +1,1 @@
+console.error("Error: Can't walk dependency graph: Cannot find module 'C:\\Users\\greta\\programming-projects\\futureproof\\chirper\\client\\static\\js\\index.js' from 'C:\\Users\\greta\\programming-projects\\futureproof\\chirper\\client\\static\\js\\_fake.js'\n    required by C:\\Users\\greta\\programming-projects\\futureproof\\chirper\\client\\static\\js\\_fake.js");
