01963d2aeeae1b39a1fd107639cf7cdc5cf1ab2b
gretaivan <greta.ivan91@gmail.com>
2021-03-16 10:19:19 +0000

    fixes index.js, package.json, adds Procfile

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000
 const Entry = require('./models/entry')
 
 
-server.listen(port, () => console.log(`\nExpress departing now from port 3000!\n`))
+server.listen(port, () => console.log(`\nExpress departing now from port ${port}!\n`))
 Entry.addReaction(1, "like")
 // Entry.addReaction(0, "dislike")
 // Entry.addReaction(3, "tree")
