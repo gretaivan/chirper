0ab239105746d0150b661fc70f9200f10d308690
gretaivan <greta.ivan91@gmail.com>
2021-03-16 12:23:29 +0000

    adds commenting feature

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const Entry = require('./models/entry')
 
 
 server.listen(port, () => console.log(`\nExpress departing now from port 3000!\n`))
-Entry.addReaction(1, "like")
-// Entry.addReaction(0, "dislike")
-// Entry.addReaction(3, "tree")
+
+
+
