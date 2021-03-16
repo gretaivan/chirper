ede535ec6856a95ff66a2b7e6a4d8a85415a046e
gretaivan <greta.ivan91@gmail.com>
2021-03-16 09:28:45 +0000

    fixes reaction bugs, refactors to the cleaner code in the entry.js file

--- models/entry.js
+++ models/entry.js
@@ -1,7 +1,7 @@
 // const DB = require('../../db.json');
 const fs = require('fs');
 
-//READ JSON file
+//READ from JSON file
 function read(){
     let entries = fs.readFileSync('../db.json', 'utf-8');
     entries = JSON.parse(entries);
@@ -19,26 +19,20 @@ function write(obj ){
 
 let entriesData = read();
 
-// console.log("entries data: ")
-// console.log(entriesData)
-
 class Entry { 
 
     constructor(data){
         this.id = data.id;
         this.entry = data.entry;
         this.date = data.date; 
-        this.reaction = [{like: data.reaction['like'] || 0}, {dislike: 0}, {tree: 0}]; 
+        this.reaction = [{like: data.reaction['like'] || 0}, {dislike: data.reaction['dislike'] || 0}, {tree: data.reaction['tree'] || 0}]; 
         this.comments = 0;
     }
 
     static create(data){
-        //date with time as a string
         let count = entriesData.length;
-        // console.log(data)
         const newEntry = new Entry({id: count, ...data}); 
         entriesData.push(newEntry);
-        // console.log(entriesData)
         write(entriesData);
         return newEntry;
     }
@@ -51,18 +45,14 @@ class Entry {
     static findById(id){
         try{
             const entry = entriesData.filter((e) => e.id === id)[0];
-            // console.log(entry)
             console.log(`ID CHECK: ${entry.id}`)
-            // if(entry.id === id){
                 return entry;
-            // }
         } catch(err){
             let errorStr = `Entry by ${id} does not exist!`;
             throw new Error(errorStr);
         }
     }
 
-    //add reaction 
     static addReaction(id, reaction){
         try {
             console.log("entering switch.")
@@ -72,24 +62,17 @@ class Entry {
                 case 'like': 
                     count = Object.values(entry.reaction[0])
                     count++;
-                    // entry.reaction[0] = count;
                     entry.reaction[0].like = count++
-                    // console.log(entry)
-                    // console.log(count)
-                    // console.log(entriesData)
-
                     break;
                 case 'dislike':
                     count = Object.values(entry.reaction[1])
                     count++;
-                    // entry.reaction[0] = count;
                     entry.reaction[1].dislike = count++
                     console.log(entry)
                     break
                 case 'tree':
                     count = Object.values(entry.reaction[2])
                     count++;
-                    // entry.reaction[0] = count;
                     entry.reaction[2].tree = count++
                     console.log(entry)
                     break
@@ -104,7 +87,6 @@ class Entry {
         }
             catch(error) {
                 console.warn()
-            
         }
     }
 
@@ -114,8 +96,5 @@ class Entry {
 
    
 }
-
-
-// let firstEntry = Entry.create({message: 'Yo whats up lets save the planet', date: '25/04/2373'})
 
 module.exports = Entry;
