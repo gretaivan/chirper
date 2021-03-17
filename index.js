9f9ec40612fe67b82322caa00008fb59f4912520
RKuppan97 <ryan_kuppan@hotmail.com>
2021-03-17 12:35:30 +0000

    client side testing: journal and format testing

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const server = require('./server')
-<<<<<<< HEAD
-const port = process.env.PORT || 4000
-const Entry = require('./models/entry')
-=======
 const port = process.env.PORT || 3000
 //const Entry = require('./models/entry')
->>>>>>> greta/staging
 
 server.listen(port, () => console.log(`\nExpress departing now from port ${port}!\n`))
 
