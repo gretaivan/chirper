5d6c356b3a244ff9e0684f1f5be9d334683907cb
gretaivan <greta.ivan91@gmail.com>
2021-03-18 22:51:46 +0000

    cleans up all server test suites

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,10 +1,7 @@
-
-// // jest.mock('../controllers/entries');
 const fs = require('fs');
 const request = require("supertest");
 const server = require('../server');
 const Entry = require('../models/entry');
-// const controller = require('../controllers/entries')
 jest.mock('../controllers/utils', () => jest.requireActual('../controllers/__mocks__/utils'))
 const utils = require('../controllers/utils');
 //test data
@@ -24,101 +21,91 @@ describe('API server test', () => {
     });
 
     afterAll(done => {
-        //console.log(`Closing test server on port ${port}`);
         apiServer.close(done);
     });
 
-describe('Route "/" ', () => { 
-    it('should GET with status code 200', done => { 
-        request(apiServer)
-            .get('/')
-            .expect(200, done) 
-    });
-    it('shoul GET and return message', done => { 
-        request(apiServer)
-            .get('/')
-            .expect("Hello there!", done)
-    });
-});  
+    describe('Route "/" ', () => { 
+        it('should GET with status code 200', done => { 
+            request(apiServer)
+                .get('/')
+                .expect(200, done) 
+        });
+        it('shoul GET and return message', done => { 
+            request(apiServer)
+                .get('/')
+                .expect("Hello there!", done)
+        });
+    });  
 
-describe('Route "/entry" ', () => { 
-     //Something wrong with callback function here? 
-     it('should POST with status 201', (done) => {
-        //test object
-        let testEntry = {
-            "entry":"Planet saving test",
-            "date":"25/04/2373"
-        }
-        //jest.setTimeout(30000);
-        request(apiServer)
-            .post('/entry') 
-            .send(testEntry) 
-            .expect(201, done) 
-            // .expect({id: 4, ...testEntry}, done) 
-    });
-    it('should POST and return a new entry', (done) => {
+    describe('Route "/entry" ', () => { 
 
-        let testEntry = testEntries[1];
+        it('should POST with status 201', (done) => {
+            //test object
+            let testEntry = {
+                "entry":"Planet saving test",
+                "date":"25/04/2373"
+            }
 
-        request(apiServer)
-            .post('/entry') 
-            .send(testEntries[1]) 
-            .expect(testEntry, done) 
-    });
+            request(apiServer)
+                .post('/entry') 
+                .send(testEntry) 
+                .expect(201, done) 
 
-    it('should GET with status 200', (done) => {
-        request(apiServer)
-            .get('/entry') 
-            .expect(200, done) 
-    });
-    
-    it('should GET and return a new entry', (done) => {
+        });
+        it('should POST and return a new entry', (done) => {
 
-        let str = Entry.all
-        str = JSON.stringify(str)
+            let testEntry = testEntries[1];
 
-        request(apiServer)
-            .get('/entry') 
-            .expect(str, done)
-    });
-});  
-    
+            request(apiServer)
+                .post('/entry') 
+                .send(testEntries[1]) 
+                .expect(testEntry, done) 
+        });
 
-describe('Route "/entry/reaction" ', () => { 
-    it('should POST with status 201', (done) => {
-     
-        request(apiServer)
-            .post('/entry/reaction') 
-            .send({id: 5, reaction: 'like'}) 
-            .expect(201, done) 
+        it('should GET with status 200', (done) => {
+            request(apiServer)
+                .get('/entry') 
+                .expect(200, done) 
+        });
+        
+        it('should GET and return a new entry', (done) => {
+
+            let str = Entry.all
+            str = JSON.stringify(str)
+
+            request(apiServer)
+                .get('/entry') 
+                .expect(str, done)
+        });
+    });  
+        
+
+    describe('Route "/entry/reaction" ', () => { 
+        it('should POST with status 201', (done) => {
+        
+            request(apiServer)
+                .post('/entry/reaction') 
+                .send({id: 5, reaction: 'like'}) 
+                .expect(201, done) 
+        })
+        it('should POST and returns the object', (done) => {
+
+            request(apiServer)
+                .post('/entry/reaction') 
+                .send({id: 5, reaction: 'like'}) 
+                .expect(Entry.findById(5), done) 
+        })
     })
-    it('should POST and returns the object', (done) => {
 
-        request(apiServer)
-            .post('/entry/reaction') 
-            .send({id: 5, reaction: 'like'}) 
-            .expect(Entry.findById(5), done) 
+    describe('Route "/entry/comment" ', () => { 
+        it('should PATCH with status 201 ', (done) => {
+
+            request(apiServer)
+                .patch('/entry/comment')
+                .send({id: 5, comment: "hello"})
+                .expect(Entry.findById(5), done)
+        })
     })
-})
-
-describe('Route "/entry/comment" ', () => { 
-    it('should PATCH with status 201 ', (done) => {
-
-        request(apiServer)
-            .patch('/entry/comment')
-            .send({id: 5, comment: "hello"})
-            .expect(Entry.findById(5), done)
-    })
-})
-    //Something wrong with callback function here? 
-//    
-
-//     it('responds to request get /entry  with status code 200', (done) => {
-//         request(apiServer)
-//             .get('/entry')
-//             .expect(200, done)
-//             //.expect(DB, done)
-//     });  
 
 });
 
