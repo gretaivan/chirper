425ae2039504327766579bbe0369af6bc18d6f3d
gretaivan <greta.ivan91@gmail.com>
2021-03-18 12:06:16 +0000

    fixes get all test completes test create new entry

--- tests/model.test.js
+++ tests/model.test.js
@@ -3,59 +3,101 @@ const Entry = require('../models/entry');
 jest.mock('../controllers/utils', () => jest.requireActual('../controllers/__mocks__/utils'))
 const utils = require('../controllers/utils')
 
-describe('Entry model tests', () => {
-    // let testEntry = jest.fn(() => {
-    let testEntry = {        
-            "entry":"Tests model entries",
-            "date":"17/3/2021 @ 13:02",
-            "reaction":[{"like":19},{"dislike":4},{"tree":1}],
-            "comments":0
-        } 
 
+
+//let allEntries = utils.read();
+
+let testEntries = [{        
+    "entry":"First test to test model entry",
+    "date":"17/3/2021 @ 13:02",
+    "reaction":[{"like":19},{"dislike":4},{"tree":1}],
+    "comments":0
+},
+{
+    "entry":"Second test to test model entry",
+    "date":"17/3/2021 @ 13:02"
+}
+]    
+
+
+describe('Create journal entry', () => {
+   
+    
+    //const mockEntriesList = jest.fn(Entry.all);
+
+   // const a = new mockFn();
+
+    //mockFn.mock.instances[0] === a; // true
+    //.mock.instances[1] === b; // true
     let allEntries = utils.read();
-    it('should return all historical journal entries', () => {
-        allEntries = utils.read();
-        expect(Entry.all.length).toBe(allEntries.length)
-    });
+    const createdEntries = testEntries.map( ent => Entry.create(ent)); 
 
-    //model.write = jest.fn();
-    it('should create a journal entry, generate id, append the all entries data array and return it ', () => {
-        const createdEntry = Entry.create(testEntry); 
-        //correct id generated
-        expect(createdEntry.id).toEqual(allEntries.length) // should have the id of all entries array length
-        //check if new element is added to the all entries array
-        expect(Entry.all.length).toBe(allEntries.length + 1)
+
+    it('should create a journal entry and return instace of Entry ', () => {
+        
+        expect(createdEntries[0]).toBeInstanceOf(Entry);        
     })
 
-    describe('should find an entry by id',()=> {
-        it('when correct id is passed it finds the entry and returns it', () => {
-           // try{
-                expect(Entry.findById(0)).toEqual(allEntries[0])
-               // expect(Entry.findById(10)).toEqual(allEntries[allEntries.length])
-          //  } catch { console.warn }
-        });
-            
-        it('when incorrect id is passed it returns message that id does not exist', () => {
-           try{
-                expect(Entry.findById(10)).toThrowError(TypeError);
-                expect(drinkOctopus).toThrowError(/^ID is out of bounds$/);
-           } 
-           catch(err){
-               console.warn
-           }
-           
-           
-           
-        //    function checkId() {
-        //         Entry.findById(10);
-        //       }
-        //     console.log( Entry.findById(10))
-            //expect(Entry.findById(10))
-            //expect(checkId)
-           
-            // expect(Entry.findById(allEntries.length+1)).toTrow();
-        });
+    it('should append to all entries data array', () => {
+        expect(Entry.all.length).toBe(allEntries.length + 2);
+        
+    })
+
+    it('should create entry ID that is corresponds to the position in the all entries record', () => {
+       
+        expect(createdEntries[1].id).toEqual(allEntries.length + 1) // should have the id of all entries array length
+        
+    })
+
+    it('should create new entry with default number of reactions', () => {
+       
+        expect(createdEntries[1].reaction[0].like).toEqual(0) // should have the id of all entries array length
+        
+    })
+
+    //Entry.deleteLastEntry()
+    //Entry.deleteLastEntry()
+});
+
+describe('Get all entries', () => {
+   
+    // fs.writeFile = jest.fn(() => {
+    //     console.log('called fn')
+    //     utils.message();
+    // })
+    // beforeEach(() => {
+    //     allEntries = utils.read();
+    // });
+
+
+    it('should return all historical journal entries', () => {
+        //let allEntries = utils.read();
+        //expect(Entry.all.length).toEqual(allEntries.length)
+        //let get = Entry.all
+        expect(Entry.all.length).toEqual(DB.length + 2) //+ 2 as we have 2 test cases
     });
+});
+   
+
+describe('should find an entry by id',()=> {
+    it('when correct id is passed it finds the entry and returns it', () => {
+        // try{
+            let allEntries = utils.read()
+            expect(Entry.findById(0)).toEqual(allEntries[0])
+            // expect(Entry.findById(10)).toEqual(allEntries[allEntries.length])
+        //  } catch { console.warn }
+    });
+        
+    it('when incorrect id is passed it returns message that id does not exist', () => {
+        try{
+            expect(Entry.findById(10)).toThrowError(TypeError);
+            expect(drinkOctopus).toThrowError(/^ID is out of bounds$/);
+        } 
+        catch(err){
+            console.warn
+        }
+    });
+});
         
     //it()
     // beforeEach(() => { 
@@ -77,7 +119,7 @@ describe('Entry model tests', () => {
     //     writeFunc = jest.fn(() => {})
     //     expect(allEntries.length).toBe(DB.length)
     // });
-});
+
 
 // describe('JSON file access', () => { 
 //     beforeEach(() => {
@@ -95,9 +137,9 @@ describe('Journal entry model tests', () => {
                 "comments":0
             }
         ]
-        })
-        //model.write = jest.fn();
-    });
+    })
+    //model.write = jest.fn();
+});
 
     // it('checks if write is mock', () => {
 
