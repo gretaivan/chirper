9ad91c6b9a142c8b2654d14fc43c1cab55d5afb3
“AdilIqbal95” <adil.iqbal@hotmail.co.uk>
2021-03-18 09:18:59 +0000

    updated journal & bundle

--- client/js/journal.js
+++ client/js/journal.js
@@ -43,7 +43,7 @@ function submitJournal(e) {
     },
   };
 
-  fetch(`${herokuURL}/entry`, options)
+  fetch(`${testingURL}/entry`, options)
     .then((r) => r.json())
     .then(appendEntry)
     .catch(console.warn);
@@ -92,7 +92,7 @@ function submitReaction(id, reaction) {
     },
   };
 
-  fetch(`${herokuURL}/entry/reaction`, options)
+  fetch(`${testingURL}/entry/reaction`, options)
     .then((r) => r.json())
     .then(updateReaction)
     .catch(console.warn);
@@ -197,7 +197,7 @@ function appendEntry(data) {
 }
 
 function requestEntries() {
-  fetch(`${herokuURL}/entry`)
+  fetch(`${testingURL}/entry`)
     .then((r) => r.json())
     .then(appendEntries)
     .catch(console.warn);
@@ -284,7 +284,7 @@ function submitComment(e) {
     },
   };
 
-  fetch(`${herokuURL}/entry/comment`, options)
+  fetch(`${testingURL}/entry/comment`, options)
     .then((r) => r.json())
     .then(updateComment)
     .catch(console.warn);
@@ -398,7 +398,7 @@ function submitGif(url) {
     },
   };
 
-  fetch(`${herokuURL}/entry`, options)
+  fetch(`${testingURL}/entry`, options)
     .then((r) => r.json())
     .then(appendEntry)
     .catch(console.warn);
